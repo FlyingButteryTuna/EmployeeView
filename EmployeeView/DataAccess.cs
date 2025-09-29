f0e09877c22869263d5fd906d74b20506b7dfb05
Arseniy Tsekh <tsekh.arseniy@yandex.ru>
2025-09-29 19:20:51 +0300

    feat(DataAccess): store db primitives into consts

--- EmployeeView/DataAccess.cs
+++ EmployeeView/DataAccess.cs
@@ -1,6 +1,9 @@
 ﻿using System.Data;
 using Microsoft.Data.SqlClient;
 
+using static EmployeeViewer.Data.DbNames;
+using static EmployeeViewer.Data.SqlParams;
+
 namespace EmployeeViewer.Data
 {
     public sealed class Db : IDisposable
@@ -28,7 +31,7 @@ namespace EmployeeViewer.Data
         {
             await using var conn = new SqlConnection(_connectionString);
             await conn.OpenAsync(ct);
-            await using var cmd = CreateProc(conn, "dbo.usp_Statuses_Get");
+            await using var cmd = CreateProc(conn, UspStatusesGet);
             await using var rdr = await cmd.ExecuteReaderAsync(ct);
 
             var list = new List<Models.LookupItem>();
@@ -41,7 +44,7 @@ namespace EmployeeViewer.Data
         {
             await using var conn = new SqlConnection(_connectionString);
             await conn.OpenAsync(ct);
-            await using var cmd = CreateProc(conn, "dbo.usp_Dependencies_Get");
+            await using var cmd = CreateProc(conn, UspDepsGet);
             await using var rdr = await cmd.ExecuteReaderAsync(ct);
 
             var list = new List<Models.LookupItem>();
@@ -54,7 +57,7 @@ namespace EmployeeViewer.Data
         {
             await using var conn = new SqlConnection(_connectionString);
             await conn.OpenAsync(ct);
-            await using var cmd = CreateProc(conn, "dbo.usp_Posts_Get");
+            await using var cmd = CreateProc(conn, UspPostsGet);
             await using var rdr = await cmd.ExecuteReaderAsync(ct);
 
             var list = new List<Models.LookupItem>();
@@ -71,28 +74,27 @@ namespace EmployeeViewer.Data
             await using var conn = new SqlConnection(_connectionString);
             await conn.OpenAsync(ct);
 
-            await using var cmd = CreateProc(conn, "dbo.usp_Persons_List_v2");
-            cmd.Parameters.Add("@StatusId", SqlDbType.Int).Value = (object?)statusId ?? DBNull.Value;
-            cmd.Parameters.Add("@DepId", SqlDbType.Int).Value = (object?)depId ?? DBNull.Value;
-            cmd.Parameters.Add("@PostId", SqlDbType.Int).Value = (object?)postId ?? DBNull.Value;
-            cmd.Parameters.Add("@LastNameLike", SqlDbType.NVarChar, 100).Value =
-                string.IsNullOrWhiteSpace(lastNameLike) ? (object)DBNull.Value : lastNameLike;
-            cmd.Parameters.Add("@SortColumn", SqlDbType.NVarChar, 50).Value = sortColumn ?? "last_name";
-            cmd.Parameters.Add("@SortDir", SqlDbType.NVarChar, 4).Value =
-                string.Equals(sortDir, "DESC", StringComparison.OrdinalIgnoreCase) ? "DESC" : "ASC";
+            await using var cmd = CreateProc(conn, UspPersonsListV2);
+            cmd.Parameters.Add(Int("@StatusId", statusId));
+            cmd.Parameters.Add(Int("@DepId", depId));
+            cmd.Parameters.Add(Int("@PostId", postId));
+            cmd.Parameters.Add(NVarChar("@LastNameLike", 100, lastNameLike));
+            cmd.Parameters.Add(NVarCharFixed("@SortColumn", 50, string.IsNullOrWhiteSpace(sortColumn) ? ColLastName : sortColumn!));
+            cmd.Parameters.Add(NVarCharFixed("@SortDir", 4,
+                string.Equals(sortDir, "DESC", StringComparison.OrdinalIgnoreCase) ? "DESC" : "ASC"));
 
             var list = new List<Models.PersonRow>();
             await using var rdr = await cmd.ExecuteReaderAsync(ct);
 
-            int ordId = rdr.GetOrdinal("id");
-            int ordLn = rdr.GetOrdinal("last_name");
-            int ordFn = rdr.GetOrdinal("first_name");
-            int ordSn = rdr.GetOrdinal("second_name");
-            int ordStatus = rdr.GetOrdinal("status_name");
-            int ordDep = rdr.GetOrdinal("dep_name");
-            int ordPost = rdr.GetOrdinal("post_name");
-            int ordDe = rdr.GetOrdinal("date_employ");
-            int ordDu = rdr.GetOrdinal("date_uneploy");
+            int ordId = rdr.GetOrdinal(ColId);
+            int ordLn = rdr.GetOrdinal(ColLastName);
+            int ordFn = rdr.GetOrdinal(ColFirstName);
+            int ordSn = rdr.GetOrdinal(ColSecondName);
+            int ordStatus = rdr.GetOrdinal(ColStatusName);
+            int ordDep = rdr.GetOrdinal(ColDepName);
+            int ordPost = rdr.GetOrdinal(ColPostName);
+            int ordDe = rdr.GetOrdinal(ColDateEmploy);
+            int ordDu = rdr.GetOrdinal(ColDateUneploy);
 
             while (await rdr.ReadAsync(ct))
             {
@@ -116,16 +118,23 @@ namespace EmployeeViewer.Data
         {
             await using var conn = new SqlConnection(_connectionString);
             await conn.OpenAsync(ct);
-            await using var cmd = CreateProc(conn, "dbo.usp_Stats_ByDay");
-            cmd.Parameters.Add("@StatusId", SqlDbType.Int).Value = statusId;
-            cmd.Parameters.Add("@DateFrom", SqlDbType.Date).Value = dateFrom.Date;
-            cmd.Parameters.Add("@DateTo", SqlDbType.Date).Value = dateTo.Date;
-            cmd.Parameters.Add("@IsHired", SqlDbType.Bit).Value = hired;
+            await using var cmd = CreateProc(conn, UspStatsByDay);
+
+            cmd.Parameters.Add(Int("@StatusId", statusId));
+            cmd.Parameters.Add(Date("@DateFrom", dateFrom));
+            cmd.Parameters.Add(Date("@DateTo", dateTo));
+            cmd.Parameters.Add(Bit("@IsHired", hired));
 
             var list = new List<Models.StatPoint>();
             await using var rdr = await cmd.ExecuteReaderAsync(ct);
             while (await rdr.ReadAsync(ct))
-                list.Add(new Models.StatPoint { Day = rdr.GetDateTime(0).Date, Count = rdr.GetInt32(1) });
+            {
+                list.Add(new Models.StatPoint
+                {
+                    Day = rdr.GetDateTime(0).Date,
+                    Count = rdr.GetInt32(1)
+                });
+            }
             return list;
         }
     }
---
+++ EmployeeView/DbPrimitives.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace EmployeeViewer.Data
+{
+    internal static class DbNames
+    {
+        public const string UspStatusesGet = "dbo.usp_Statuses_Get";
+        public const string UspPostsGet = "dbo.usp_Posts_Get";
+        public const string UspDepsGet = "dbo.usp_Dependencies_Get";
+        public const string UspPersonsListV2 = "dbo.usp_Persons_List_v2";
+        public const string UspStatsByDay = "dbo.usp_Stats_ByDay";
+
+        public const string ColId = "id";
+        public const string ColFirstName = "first_name";
+        public const string ColSecondName = "second_name";
+        public const string ColLastName = "last_name";
+        public const string ColStatusName = "status_name";
+        public const string ColDepName = "dep_name";
+        public const string ColPostName = "post_name";
+        public const string ColDateEmploy = "date_employ";
+        public const string ColDateUneploy = "date_uneploy";
+        public const string ColDay = "Day";
+        public const string ColCount = "Cnt";
+    }
+
+    internal static class SqlParams
+    {
+        public static SqlParameter Int(string name, int? value)
+        {
+            var p = new SqlParameter(name, SqlDbType.Int);
+            p.Value = (object?)value ?? DBNull.Value;
+            return p;
+        }
+
+        public static SqlParameter Bit(string name, bool value) => new SqlParameter(name, SqlDbType.Bit) { Value = value };
+
+        public static SqlParameter Date(string name, DateTime value) => new SqlParameter(name, SqlDbType.Date) { Value = value.Date };
+
+        public static SqlParameter NVarChar(string name, int size, string? value)
+        {
+            var p = new SqlParameter(name, SqlDbType.NVarChar, size);
+            p.Value = string.IsNullOrWhiteSpace(value) ? (object)DBNull.Value : value!;
+            return p;
+        }
+
+        public static SqlParameter NVarCharFixed(string name, int size, string value) =>
+            new SqlParameter(name, SqlDbType.NVarChar, size) { Value = value };
+    }
+}
